6a039b85eb2a763aa9dbfb9e53f1ab85269506d5
Dhruv-HM <dhruv.kanthaliya@happiestminds.com>
2023-01-19 00:19:23 +0530

    buttons

--- src/pages/tableView2.js
+++ src/pages/tableView2.js
@@ -7,7 +7,11 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import JsonData from "../data.json"
+import JsonData from "../data.json";
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -37,6 +41,7 @@ const TablePage = () => {
     const classes = useStyles();
     const [product, setProduct] = useState([]);
     const [search, setSearch] = useState("");
+    const[sliceIndex, setNext] = useState(1);
 
     const getProductData = async () => {
         try {
@@ -47,6 +52,16 @@ const TablePage = () => {
             console.log(e);
         }
     };
+
+    function next(){
+        setNext(sliceIndex+2);
+        console.log(sliceIndex);
+    }
+
+    function previous(){
+        setNext(sliceIndex-2);
+        console.log(sliceIndex)
+    }
 
     useEffect(() => {
         getProductData();
@@ -62,12 +77,15 @@ const TablePage = () => {
 
                 style={{margin:"1%"}}
             />
+            <button onclick={previous} style={{backgroundColor:"transparent",border:0}}><ArrowBackIosIcon/></button>
 
-
+            <button onClick={next} style={{backgroundColor:"transparent",border:0}}><ArrowForwardIosIcon/></button>
             <TableContainer component={Paper}>
                 <Table className={classes.table} aria-label="customized table">
                     <TableHead>
                         <TableRow>
+                            <StyledTableCell>Product</StyledTableCell>
+
                             <StyledTableCell>Product Name</StyledTableCell>
                             <StyledTableCell >Product Details</StyledTableCell>
                             <StyledTableCell >Product Price</StyledTableCell>
@@ -75,7 +93,7 @@ const TablePage = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {product
+                        {JsonData
                             .filter((item) => {
                                 if (search == "") {
                                     return item;
@@ -89,6 +107,9 @@ const TablePage = () => {
                                 return (
                                     <StyledTableRow key={item.id}>
                                         <StyledTableCell component="th" scope="row">
+                                            <img src={item.image} style={{height:"80px", borderRadius:"10%"}}></img> 
+                                        </StyledTableCell>
+                                        <StyledTableCell component="th" scope="row">
                                             {item.productName}
                                         </StyledTableCell>
 
@@ -101,7 +122,7 @@ const TablePage = () => {
                                         </StyledTableCell>
                                     </StyledTableRow>
                                 );
-                            })}
+                            }).slice(sliceIndex,sliceIndex+4)}
                     </TableBody>
                 </Table>
             </TableContainer>
